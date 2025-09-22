5936d35121dc487223a2b177e2576875291c81db
Diluc99 <jhashivam53741@gmail.com>
2025-09-22 14:56:45 +0530

    Reading Raw data from MPU6050

---
+++ TestComp/TestMPU6050/esp32MPU6050testing/esp32MPU6050testing.ino
@@ -0,0 +1,31 @@
+#include<MPU6050.h>
+#include<Wire.h>
+MPU6050 mpu;
+void setup() {
+Serial.begin(115200);
+Wire.begin(21,22);
+mpu.initialize();
+if(mpu.testConnection()){
+  Serial.println("Connected");
+}else{
+  Serial.println("Not Connected");
+  while(1);
+}
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int16_t ax,ay,az; // accelerometer values
+  int16_t gx,gy,gz; // gyroscope values
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  Serial.print("Accel: ");
+  Serial.print(ax); Serial.print(", ");
+  Serial.print(ay); Serial.print(", ");
+  Serial.print(az); Serial.print(" | ");
+   Serial.print("Gyro: ");
+  Serial.print(gx); Serial.print(", ");
+  Serial.print(gy); Serial.print(", ");
+  Serial.println(gz);
+  delay(250); 
+}
